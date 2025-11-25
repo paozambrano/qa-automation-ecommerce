e8ab105a70e00ef198c9a2629c0ea54f2ddfb436
Pao Zambrano <pao.zambranoo_@hotmail.com>
2025-11-25 16:27:36 -0500

    Implement CartPage class with methods for navigating to the cart and checking out; add corresponding test case

---
+++ ui-tests/tests/test_cart.py
@@ -0,0 +1,15 @@
+from pages.login_page import LoginPage
+from pages.cart_page import CartPage
+
+def test_cart_checkOut (driver, base_url):
+    driver.get(base_url)
+
+    lp = LoginPage(driver)
+    lp.login("standard_user", "secret_sauce")
+
+    cp = CartPage(driver)
+    cp.goToCart()
+    cp.checkOut()
+    cp.checkOut_info('Paola', 'Zambrano', 18061)
+
+    assert 'checkout-step-two' in driver.current_url
