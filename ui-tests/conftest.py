4b877bc5c635b1afc0225c59f226fab2f1c39b65
Pao Zambrano <pao.zambranoo_@hotmail.com>
2025-11-26 10:38:45 -0500

    Refactor driver setup to use Firefox instead of Chrome in conftest.py

--- ui-tests/conftest.py
+++ ui-tests/conftest.py
@@ -1,12 +1,12 @@
 import pytest
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.service import Service as FirefoxService
 
 @pytest.fixture(scope='session')
 def driver():
-    service = ChromeService(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
+    service = FirefoxService(GeckoDriverManager().install())
+    driver = webdriver.Firefox(service=service)
     driver.maximize_window()
     yield driver
     driver.quit()
